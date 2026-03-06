16b1e873653984e5f9e2466650598a67746a14a5
AniketKasurde <aniketkasurde14@gmail.com>
2026-03-06 11:44:39 +0530

    initial defined variables

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "projec_name" {
+  description = "name of the project"
+  type        = string
+  default     = "flask-eks-pipeline"
+}
+
