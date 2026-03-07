1f01df4051a39510c0f51957e255e7cc994459d8
AniketKasurde <aniketkasurde14@gmail.com>
2026-03-07 11:03:48 +0530

    removed aws_region var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "aws region"
-  type        = string
-  default     = "ap-south-1"
-}
-
 variable "project_name" {
   description = "name of the project"
   type        = string
