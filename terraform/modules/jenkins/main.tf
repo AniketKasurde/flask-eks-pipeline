c118c82fdcda93f3a76e54c8a69b1bc424cdbcf2
AniketKasurde <aniketkasurde14@gmail.com>
2026-03-07 11:00:45 +0530

    added quotes around ami_id & instance_type

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "jenkins" {
 
 #Jenkins EC2
 resource "aws_instance" "jenkins" {
-  ami                    = ami-019715e0d74f695be
-  instance_type          = t3.small
+  ami                    = "ami-019715e0d74f695be"
+  instance_type          = "t3.small"
   subnet_id              = var.public_subnet_id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.jenkins.id]
